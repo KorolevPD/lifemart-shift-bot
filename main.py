8671198c4e794f59182067dba9be51c25c0c67b5
Pavel Korolev <p.koror56@gmail.com>
2025-11-09 18:41:25 +0500

    Refactor main.py to improve database connection handling and update bot initialization

--- main.py
+++ main.py
@@ -1,29 +1,52 @@
 import asyncio
 import logging
+
 from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.fsm.storage.memory import MemoryStorage
+from sqlalchemy import text
 
+from bot.handlers import common
+from bot.middlewares.access import AccessMiddleware
 from infra.config import settings
+from infra.db.database import engine, init_db
 from infra.logger import setup_logging
-from bot.handlers import admin, schedule, common
 
 
 async def main():
+
     # Logging setup
     setup_logging()
     logging.info("Bot starts...")
 
+    # Database connection test
+    try:
+        async with engine.begin() as conn:
+            await conn.execute(text("SELECT 1"))
+        logging.info("Database successfully connected")
+    except Exception as e:
+        logging.exception(f"Database connection error:\n{e}")
+        return
+
+    # Database initialization
+    await init_db()
+    logging.info("Database and tables are successfuly created")
+
     # Bot initialization
-    bot = Bot(token=settings.BOT_TOKEN, parse_mode=ParseMode.HTML)
+    bot = Bot(token=settings.BOT_TOKEN,
+              default=DefaultBotProperties(parse_mode=ParseMode.HTML))
     dp = Dispatcher(storage=MemoryStorage())
 
     # Handlers registration
     dp.include_routers(
-        admin.router,
-        schedule.router,
+        # admin.router,
+        # schedule.router,
         common.router,
     )
+
+    # Add user access middleware
+    dp.message.middleware(AccessMiddleware())
 
     # Log notification
     logging.info("Bot started successfully. Waiting for messages...")
